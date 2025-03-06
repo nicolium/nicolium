d0a0912e3fc7bbec0b998af450bbad6427d870ac
mkljczk <git@mkljczk.pl>
2025-03-06 11:43:09 +0100

    pl-fe: actually include the license text
    Signed-off-by: mkljczk <git@mkljczk.pl>

--- packages/pl-fe/src/utils/url-purify.ts
+++ packages/pl-fe/src/utils/url-purify.ts
@@ -9,7 +9,7 @@
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
 //
 // I hope I got this relicensing stuff right xD
 import { URLPurify, type SerializedRules } from '@mkljczk/url-purify';
