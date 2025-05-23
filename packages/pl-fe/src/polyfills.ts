edf105bfc9197d1e0e156a47d222be95fdde776f
Nicole Mikołajczyk <git@mkljczk.pl>
2025-05-23 17:26:47 +0200

    pl-fe: Replace resize observer polyfill
    Signed-off-by: Nicole Mikołajczyk <git@mkljczk.pl>

--- packages/pl-fe/src/polyfills.ts
+++ packages/pl-fe/src/polyfills.ts
@@ -1,8 +1,8 @@
 import 'abortcontroller-polyfill/dist/polyfill-patch-fetch';
 import 'intersection-observer';
-import ResizeObserver from 'resize-observer-polyfill';
+import { install as installResizeObserver } from 'resize-observer';
 
 // Needed by @tanstack/virtual, I guess
 if (!window.ResizeObserver) {
-  window.ResizeObserver = ResizeObserver;
+  installResizeObserver();
 }
