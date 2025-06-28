8dd657a043ded83117402e38306c1e4ae46eba9c
nicole mikołajczyk <git@mkljczk.pl>
2025-06-28 13:49:45 +0200

    pl-fe: a11y: add explanation to character counter
    Signed-off-by: nicole mikołajczyk <git@mkljczk.pl>

--- packages/pl-fe/src/features/compose/components/text-character-counter.tsx
+++ packages/pl-fe/src/features/compose/components/text-character-counter.tsx
@@ -1,6 +1,11 @@
 import clsx from 'clsx';
 import React from 'react';
+import { defineMessages, useIntl } from 'react-intl';
 import { length } from 'stringz';
+
+const messages = defineMessages({
+  characterCount: { id: 'compose_form.remaining_character_count', defaultMessage: 'Remaining characters: {value} of {max}' },
+});
 
 interface ITextCharacterCounter {
   max: number;
@@ -8,12 +13,15 @@ interface ITextCharacterCounter {
 }
 
 const TextCharacterCounter: React.FC<ITextCharacterCounter> = ({ text, max }) => {
+  const intl = useIntl();
+
   const checkRemainingText = (diff: number) => (
     <span
       className={clsx('text-sm font-medium', {
         'text-gray-700': diff >= 0,
         'text-secondary-600': diff < 0,
       })}
+      title={intl.formatMessage(messages.characterCount, { value: diff, max })}
     >
       {diff}
     </span>
