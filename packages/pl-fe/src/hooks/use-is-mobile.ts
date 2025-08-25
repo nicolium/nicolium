9c4914f7efb669a81b222a7af77c6ff51fd41509
nicole mikołajczyk <git@mkljczk.pl>
2025-08-25 12:01:31 +0200

    pl-fe: remove unused
    Signed-off-by: nicole mikołajczyk <git@mkljczk.pl>

--- packages/pl-fe/src/hooks/use-is-mobile.ts
+++
@@ -1,8 +0,0 @@
-import { LAYOUT_BREAKPOINT } from 'pl-fe/is-mobile';
-
-import { useScreenWidth } from './use-screen-width';
-
-export function useIsMobile() {
-  const screenWidth = useScreenWidth();
-  return screenWidth <= LAYOUT_BREAKPOINT;
-}
