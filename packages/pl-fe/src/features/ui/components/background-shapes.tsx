33090eeff4b47f9c705680a8651dd7e398e92acc
nicole mikołajczyk <git@mkljczk.pl>
2025-10-15 17:36:27 +0200

    pl-fe: wtf
    Signed-off-by: nicole mikołajczyk <git@mkljczk.pl>

--- packages/pl-fe/src/features/ui/components/background-shapes.tsx
+++ packages/pl-fe/src/features/ui/components/background-shapes.tsx
@@ -9,10 +9,9 @@ interface IBackgroundShapes {
 }
 
 /** Gradient that appears in the background of the UI. */
-const BackgroundShapes: React.FC<IBackgroundShapes> = ({ position = 'fixed', hidden }) => (
+const BackgroundShapes: React.FC<IBackgroundShapes> = ({ position = 'fixed', hidden }) => hidden ? null : (
   <div
     className={clsx(position, 'pointer-events-none inset-x-0 top-0 flex justify-center overflow-hidden ', {
-      'hidden': hidden,
       'black:hidden': hidden === undefined,
     })}
   >
