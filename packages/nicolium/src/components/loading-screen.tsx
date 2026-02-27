0cb2607947c43b41f59f52e4395a70183dbdb08c
nicole mikołajczyk <git@mkljczk.pl>
2026-02-27 15:26:57 +0100

    nicolium: add role to loading components
    Signed-off-by: nicole mikołajczyk <git@mkljczk.pl>

--- packages/nicolium/src/components/loading-screen.tsx
+++ packages/nicolium/src/components/loading-screen.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 /** Fullscreen loading indicator. */
 const LoadingScreen: React.FC = () => (
-  <div className='loading-indicator-wrapper'>
+  <div className='loading-indicator-wrapper' role='status'>
     <div className='loading-indicator'>
       <div className='loading-indicator__container'>
         <div className='loading-indicator__figure' />
