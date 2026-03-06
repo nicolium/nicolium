7a6ec96e9fee32e64273a591a175a9fa6e18801e
nicole mikołajczyk <git@mkljczk.pl>
2026-03-06 16:46:47 +0100

    nicolium: remove polyfills because they're no longer needed
    Signed-off-by: nicole mikołajczyk <git@mkljczk.pl>

--- packages/nicolium/src/polyfills.ts
+++
@@ -1,8 +0,0 @@
-// TOOD Verify if any of the lesser-known browsers actually need those.
-import 'intersection-observer';
-import { install as installResizeObserver } from 'resize-observer';
-
-// Needed by Virtuoso.
-if (!window.ResizeObserver) {
-  installResizeObserver();
-}
