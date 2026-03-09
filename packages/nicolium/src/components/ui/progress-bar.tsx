8a95b050eb05426799478598a2d9aa1108a11b7c
nicole mikołajczyk <git@mkljczk.pl>
2026-03-09 22:56:40 +0100

    nicolium: migrate progress bar component
    Signed-off-by: nicole mikołajczyk <git@mkljczk.pl>

--- packages/nicolium/src/components/ui/progress-bar.tsx
+++ packages/nicolium/src/components/ui/progress-bar.tsx
@@ -23,12 +23,7 @@ const ProgressBar: React.FC<IProgressBar> = ({ progress, size = 'md' }) => {
   });
 
   return (
-    <div
-      className={clsx('h-2.5 w-full overflow-hidden rounded-lg bg-gray-300 dark:bg-primary-800', {
-        'h-2.5': size === 'md',
-        'h-[6px]': size === 'sm',
-      })}
-    >
+    <div className={clsx('⁂-progress-bar', { '⁂-progress-bar--sm': size === 'sm' })}>
       <animated.div className='h-full bg-secondary-500' style={styles} />
     </div>
   );
