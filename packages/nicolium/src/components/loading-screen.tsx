1dc99499ebb18addbc60e2b714e5a3b4d35f5664
nicole mikołajczyk <git@mkljczk.pl>
2026-03-17 18:04:12 +0100

    nicolium: consistency
    Signed-off-by: nicole mikołajczyk <git@mkljczk.pl>

--- packages/nicolium/src/components/loading-screen.tsx
+++ packages/nicolium/src/components/loading-screen.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import { defineMessages, useIntl } from 'react-intl';
+
+const messages = defineMessages({
+  loading: { id: 'loading_indicator.label', defaultMessage: 'Loading…' },
+});
 
 /** Fullscreen loading indicator. */
-const LoadingScreen: React.FC = () => (
-  <div className='loading-indicator-wrapper' role='status'>
-    <div className='loading-indicator'>
-      <div className='loading-indicator__container'>
-        <div className='loading-indicator__figure' />
+const LoadingScreen: React.FC = () => {
+  const intl = useIntl();
+  return (
+    <div
+      className='loading-indicator-wrapper'
+      role='status'
+      aria-label={intl.formatMessage(messages.loading)}
+    >
+      <div className='loading-indicator'>
+        <div className='loading-indicator__container'>
+          <div className='loading-indicator__figure' />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export { LoadingScreen as default };
