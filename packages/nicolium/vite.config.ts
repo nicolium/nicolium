75f9041948c9e1812bac52a8e748fb7b395114d4
nicole mikołajczyk <git@mkljczk.pl>
2026-03-22 17:59:50 +0100

    nicolium: adapt to vite-static-copy update
    Signed-off-by: nicole mikołajczyk <git@mkljczk.pl>

--- packages/nicolium/vite.config.ts
+++ packages/nicolium/vite.config.ts
@@ -128,32 +128,39 @@ const config = defineConfig(() => ({
     viteStaticCopy({
       targets: [
         {
-          src: './node_modules/@twemoji/svg/*',
+          src: './node_modules/@twemoji/svg/*.svg',
           dest: 'packs/emoji/',
+          rename: { stripBase: true },
         },
         {
           src: './favicon.ico',
           dest: '.',
+          rename: { stripBase: true },
         },
         {
           src: './favicon.svg',
           dest: '.',
+          rename: { stripBase: true },
         },
         {
           src: './src/instance',
           dest: '.',
+          rename: { stripBase: true },
         },
         {
           src: './custom/instance',
           dest: '.',
+          rename: { stripBase: true },
         },
         {
           src: './node_modules/fasttext.wasm.js/dist/models/language-identification/assets/lid.176.ftz',
           dest: 'fastText/models/',
+          rename: { stripBase: true },
         },
         {
           src: './node_modules/fasttext.wasm.js/dist/core/fastText.common.wasm',
           dest: 'fastText/',
+          rename: { stripBase: true },
         },
       ],
     }),
