de63821a85a46e68765f4626d7a37aeef60f32f2
nicole mikołajczyk <git@mkljczk.pl>
2026-03-22 21:57:01 +0100

    nicolium: styles
    Signed-off-by: nicole mikołajczyk <git@mkljczk.pl>

--- packages/nicolium/src/components/ui/multiselect.tsx
+++ packages/nicolium/src/components/ui/multiselect.tsx
@@ -23,7 +23,7 @@ THE SOFTWARE.
  */
 // Adapted from [multiselect-react-dropdown](https://github.com/srigar/multiselect-react-dropdown)
 
-import iconXCircle from '@phosphor-icons/core/regular/x-circle.svg';
+import iconX from '@phosphor-icons/core/regular/x.svg';
 import clsx from 'clsx';
 import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { defineMessages, FormattedMessage, useIntl } from 'react-intl';
@@ -235,7 +235,7 @@ const Multiselect: React.FC<IMultiselect> = ({
                 title={intl.formatMessage(messages.removeItem)}
                 aria-label={intl.formatMessage(messages.removeItem)}
               >
-                <Icon className='ml-1 size-4 hover:cursor-pointer' src={iconXCircle} />
+                <Icon className='ml-1 size-4 hover:cursor-pointer' src={iconX} />
               </button>
             </span>
           ))}
