b3b79a88cc0c78d2ed0ec5650952604d8c62b776
nicole mikołajczyk <git@mkljczk.pl>
2026-03-31 22:21:48 +0000

    nicolium: remove unused
    Signed-off-by: nicole mikołajczyk <git@mkljczk.pl>

--- packages/nicolium/src/features/emoji/containers/emoji-picker-dropdown-container.tsx
+++ packages/nicolium/src/features/emoji/containers/emoji-picker-dropdown-container.tsx
@@ -1,6 +1,5 @@
 import { useFloating, shift, flip, autoUpdate } from '@floating-ui/react';
 import iconSmiley from '@phosphor-icons/core/regular/smiley.svg';
-import clsx from 'clsx';
 import React, { useMemo, useState } from 'react';
 import { defineMessages, useIntl } from 'react-intl';
 
@@ -22,11 +21,9 @@ interface IEmojiPickerDropdownContainer extends Pick<
   'onPickEmoji' | 'condensed' | 'withCustom'
 > {
   children?: React.JSX.Element;
-  theme?: 'default' | 'inverse';
 }
 
 const EmojiPickerDropdownContainer: React.FC<IEmojiPickerDropdownContainer> = ({
-  theme = 'default',
   children,
   ...props
 }) => {
@@ -87,12 +84,7 @@ const EmojiPickerDropdownContainer: React.FC<IEmojiPickerDropdownContainer> = ({
       {clonedChildren ?? (
         <IconButton
           theme='transparent'
-          className={clsx('emoji-picker-dropdown -m-1 p-2', {
-            'bg-transparent text-gray-600 hover:bg-primary-100 hover:text-gray-800 black:hover:bg-gray-800 dark:hover:bg-primary-800 dark:hover:text-white':
-              theme === 'default',
-            'bg-transparent text-white/80 hover:text-white dark:bg-transparent':
-              theme === 'inverse',
-          })}
+          className='emoji-picker-dropdown -m-1 bg-transparent p-2 text-gray-600 hover:bg-primary-100 hover:text-gray-800 black:hover:bg-gray-800 dark:hover:bg-primary-800 dark:hover:text-white'
           ref={refs.setReference}
           src={iconSmiley}
           title={title}
