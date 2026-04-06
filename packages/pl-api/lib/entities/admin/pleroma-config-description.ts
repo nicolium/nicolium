83a016de04cb4fd4ef6bd4f929f1d5425b4098d7
nicole mikołajczyk <git@mkljczk.pl>
2026-04-06 09:55:52 +0000

    pl-api: fix pleroma config description schema
    Signed-off-by: nicole mikołajczyk <git@mkljczk.pl>

--- packages/pl-api/lib/entities/admin/pleroma-config-description.ts
+++ packages/pl-api/lib/entities/admin/pleroma-config-description.ts
@@ -6,7 +6,7 @@ import * as v from 'valibot';
 const pleromaConfigDescriptionChildSchema: v.GenericSchema<PleromaConfigDescriptionChild> =
   v.looseObject({
     key: v.optional(v.string()),
-    type: v.union([v.string(), v.array(v.string())]),
+    type: v.union([v.string(), v.array(v.union([v.string(), v.array(v.string())]))]),
     description: v.optional(v.string()),
     label: v.optional(v.string()),
     suggestions: v.optional(v.array(v.any())),
@@ -38,12 +38,12 @@ type PleromaConfigDescription = v.InferOutput<typeof pleromaConfigDescriptionSch
  */
 type PleromaConfigDescriptionChild = {
   key?: string;
-  type: string | string[];
+  type: string | Array<string | Array<string>>;
   description?: string;
   label?: string;
-  suggestions?: unknown[];
-  children?: PleromaConfigDescriptionChild[];
-  group?: string | string[];
+  suggestions?: Array<unknown>;
+  children?: Array<PleromaConfigDescriptionChild>;
+  group?: string | Array<string>;
 };
 
 export {
