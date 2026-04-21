f138fd818ea62318c94a9f252907284e297639fe
nicole mikołajczyk <git@mkljczk.pl>
2026-04-21 18:47:57 +0000

    nicolium: mark status as deleted when deleted by admin
    Signed-off-by: nicole mikołajczyk <git@mkljczk.pl>

--- packages/nicolium/src/queries/admin/use-statuses.ts
+++ packages/nicolium/src/queries/admin/use-statuses.ts
@@ -2,6 +2,7 @@ import { useMutation } from '@tanstack/react-query';
 
 import { useClient } from '@/hooks/use-client';
 import { importEntities } from '@/queries/utils/import-entities';
+import { useStatusMetaActions } from '@/stores/status-meta';
 import { useTimelinesActions } from '@/stores/timelines';
 
 import type { AdminUpdateStatusParams } from 'pl-api';
@@ -9,12 +10,14 @@ import type { AdminUpdateStatusParams } from 'pl-api';
 const useAdminDeleteStatusMutation = (statusId: string) => {
   const client = useClient();
   const { deleteStatus: deleteTimelineStatus } = useTimelinesActions();
+  const { markStatusDeleted } = useStatusMetaActions();
 
   return useMutation({
     mutationKey: ['admin', 'statuses', statusId],
     mutationFn: () => client.admin.statuses.deleteStatus(statusId),
     onSuccess: () => {
       deleteTimelineStatus(statusId);
+      markStatusDeleted(statusId);
     },
   });
 };
