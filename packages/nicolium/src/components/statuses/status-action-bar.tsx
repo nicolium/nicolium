cabda0567017043fb30ff09ff54928d2b3185e15
nicole mikołajczyk <git@mkljczk.pl>
2026-04-26 14:05:03 +0000

    nicolium: fix status action menu being ugly sometimes
    Signed-off-by: nicole mikołajczyk <git@mkljczk.pl>

--- packages/nicolium/src/components/statuses/status-action-bar.tsx
+++ packages/nicolium/src/components/statuses/status-action-bar.tsx
@@ -1162,7 +1162,7 @@ const MenuButtonRemainingItems: React.FC<IMenuButtonRemainingItems> = ({
     return STATUS_ACTIONS.filter((action) => {
       if (statusActionBarItems.includes(action)) return false;
       if (action === 'quote' && statusActionBarItems.includes('reblog')) return false;
-      if (['wrench', 'bookmark', 'share'].includes(action)) return false;
+      if (['wrench', 'bookmark', 'share', 'translate'].includes(action)) return false;
       return true;
     });
   }, [statusActionBarItems]);
