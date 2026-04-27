2fe81c1fc616b7e2375658733dd5826922969436
nicole mikołajczyk <git@mkljczk.pl>
2026-04-27 15:15:52 +0000

    nicolium: style migration
    Signed-off-by: nicole mikołajczyk <git@mkljczk.pl>

--- packages/nicolium/src/components/outline-box.tsx
+++ packages/nicolium/src/components/outline-box.tsx
@@ -8,13 +8,7 @@ interface IOutlineBox extends React.HTMLAttributes<HTMLDivElement> {
 
 /** Wraps children in a container with an outline. */
 const OutlineBox: React.FC<IOutlineBox> = ({ children, className, ...rest }) => (
-  <div
-    className={clsx(
-      'rounded-lg border border-solid border-gray-300 p-4 dark:border-gray-800',
-      className,
-    )}
-    {...rest}
-  >
+  <div className={clsx('⁂-outline-box', className)} {...rest}>
     {children}
   </div>
 );
