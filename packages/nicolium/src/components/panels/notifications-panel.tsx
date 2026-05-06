bce7b8d45c0c3d3d96124a05e1e047a9007181a2
nicole mikołajczyk <git@mkljczk.pl>
2026-05-06 00:38:59 +0000

    nicolium: widget component improvements
    Signed-off-by: nicole mikołajczyk <git@mkljczk.pl>

--- packages/nicolium/src/components/panels/notifications-panel.tsx
+++ packages/nicolium/src/components/panels/notifications-panel.tsx
@@ -1,10 +1,7 @@
-import iconCaretRight from '@phosphor-icons/core/regular/caret-right.svg';
-import { Link } from '@tanstack/react-router';
 import React from 'react';
 import { defineMessages, FormattedMessage, useIntl } from 'react-intl';
 
 import NotificationsColumn from '@/columns/notifications';
-import Icon from '@/components//ui/icon';
 import Widget from '@/components/ui/widget';
 
 const messages = defineMessages({
@@ -18,11 +15,8 @@ const NotificationsPanel: React.FC = () => {
     <Widget
       className='⁂-notifications-panel'
       title={<FormattedMessage id='column.notifications' defaultMessage='Notifications' />}
-      action={
-        <Link to='/notifications' title={intl.formatMessage(messages.viewAll)}>
-          <Icon src={iconCaretRight} aria-hidden />
-        </Link>
-      }
+      to='/notifications'
+      actionTitle={intl.formatMessage(messages.viewAll)}
     >
       <NotificationsColumn multiColumn compact />
     </Widget>
