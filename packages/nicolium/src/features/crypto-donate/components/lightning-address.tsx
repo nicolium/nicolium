5d8093bd6bea3787443ecbfaffb0ff3e616e5964
nicole mikołajczyk <git@mkljczk.pl>
2026-05-06 14:01:47 +0000

    nicolium: style migrations
    Signed-off-by: nicole mikołajczyk <git@mkljczk.pl>

--- packages/nicolium/src/features/crypto-donate/components/lightning-address.tsx
+++ packages/nicolium/src/features/crypto-donate/components/lightning-address.tsx
@@ -3,23 +3,19 @@ import { FormattedMessage } from 'react-intl';
 
 import CopyableInput from '@/components/copyable-input';
 import Emoji from '@/components/ui/emoji';
-import Text from '@/components/ui/text';
 
 interface ILightningAddress {
   address: string;
 }
 
 const LightningAddress: React.FC<ILightningAddress> = ({ address }) => (
-  <div className='flex flex-col'>
-    <div className='mb-1 flex items-center'>
-      <Emoji
-        className='mr-2.5 flex w-6 items-start justify-center rtl:ml-2.5 rtl:mr-0'
-        emoji='⚡'
-      />
+  <div className='⁂-crypto-address'>
+    <div className='⁂-crypto-address__label'>
+      <Emoji className='⁂-crypto-address__icon' emoji='⚡' />
 
-      <Text weight='bold'>
+      <p>
         <FormattedMessage id='crypto.lightning' defaultMessage='Lightning' />
-      </Text>
+      </p>
     </div>
 
     <CopyableInput value={address} />
