16e9e2291e76a34c138dea5fcc0b87bfdc53612e
nicole mikołajczyk <git@mkljczk.pl>
2026-06-10 13:30:07 +0000

    nicolium: perf
    Signed-off-by: nicole mikołajczyk <git@mkljczk.pl>

--- packages/nicolium/src/features/crypto-donate/components/crypto-icon.tsx
+++ packages/nicolium/src/features/crypto-donate/components/crypto-icon.tsx
@@ -1,12 +1,15 @@
 import genericIcon from 'cryptocurrency-icons/svg/color/generic.svg';
 import React from 'react';
 
+let modules: Record<string, any> | null = null;
+
 /** Get crypto icon URL by ticker symbol, or fall back to generic icon */
 const getIcon = (ticker: string): string => {
-  const modules: Record<string, any> = import.meta.glob(
-    '../../../../node_modules/cryptocurrency-icons/svg/color/*.svg',
-    { eager: true },
-  );
+  if (modules === null) {
+    modules = import.meta.glob('../../../../node_modules/cryptocurrency-icons/svg/color/*.svg', {
+      eager: true,
+    });
+  }
   const key = `../../../../node_modules/cryptocurrency-icons/svg/color/${ticker}.svg`;
   return modules[key]?.default ?? genericIcon;
 };
