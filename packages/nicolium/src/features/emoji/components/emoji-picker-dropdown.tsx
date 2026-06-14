d7217284066890be13daf8208cbc2e5bf37668a1
nicole mikołajczyk <git@mkljczk.pl>
2026-06-14 17:38:02 +0000

    nicolium: remove console.log
    Signed-off-by: nicole mikołajczyk <git@mkljczk.pl>

--- packages/nicolium/src/features/emoji/components/emoji-picker-dropdown.tsx
+++ packages/nicolium/src/features/emoji/components/emoji-picker-dropdown.tsx
@@ -165,7 +165,6 @@ const EmojiPickerDropdown: React.FC<IEmojiPickerDropdown> = ({
     saveSettings();
 
     if (onPickEmoji) {
-      console.log(pickedEmoji);
       onPickEmoji(pickedEmoji);
     }
   };
