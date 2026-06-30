6b22e9ee7857d961020a522675445052f44d09d9
nicole mikołajczyk <git@mkljczk.pl>
2026-06-30 12:03:27 +0000

    pl-api: include list emojis
    Signed-off-by: nicole mikołajczyk <git@mkljczk.pl>

--- packages/pl-api/lib/entities/list.ts
+++ packages/pl-api/lib/entities/list.ts
@@ -7,15 +7,28 @@ import { filteredArray } from './utils';
  * @category Schemas
  * @see {@link https://docs.joinmastodon.org/entities/List/}
  */
-const listSchema = v.object({
-  id: v.pipe(v.unknown(), v.transform(String)),
-  title: v.string(),
-  replies_policy: v.fallback(v.optional(v.picklist(['none', 'list', 'followed'])), undefined),
-  exclusive: v.fallback(v.optional(v.boolean()), undefined),
-  antennas: filteredArray(v.lazy(() => antennaSchema)),
-  notify: v.fallback(v.optional(v.boolean()), undefined),
-  favourite: v.fallback(v.optional(v.boolean()), undefined),
-});
+const listSchema = v.pipe(
+  v.any(),
+  v.transform((list: any) => {
+    if (!list) return null;
+
+    return {
+      ...list.pleroma,
+      ...list,
+    };
+  }),
+  v.object({
+    id: v.pipe(v.unknown(), v.transform(String)),
+    title: v.string(),
+    replies_policy: v.fallback(v.optional(v.picklist(['none', 'list', 'followed'])), undefined),
+    exclusive: v.fallback(v.optional(v.boolean()), undefined),
+    antennas: filteredArray(v.lazy(() => antennaSchema)),
+    notify: v.fallback(v.optional(v.boolean()), undefined),
+    favourite: v.fallback(v.optional(v.boolean()), undefined),
+    emoji: v.fallback(v.nullable(v.string()), null),
+    emoji_url: v.fallback(v.nullable(v.string()), null),
+  }),
+);
 
 /**
  * @category Entity types
