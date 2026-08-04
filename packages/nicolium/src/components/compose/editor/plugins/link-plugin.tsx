d4d9dac09c2932dfe2c2ffc8d3fa5387f01a3ce2
nicole mikołajczyk <git@mkljczk.pl>
2026-08-04 20:47:07 +0000

    compose: modify url regex
    Signed-off-by: nicole mikołajczyk <git@mkljczk.pl>

--- packages/nicolium/src/components/compose/editor/plugins/link-plugin.tsx
+++ packages/nicolium/src/components/compose/editor/plugins/link-plugin.tsx
@@ -9,7 +9,7 @@ import * as React from 'react';
 
 // Source: https://stackoverflow.com/a/8234912/2013580
 const urlRegExp = new RegExp(
-  /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[\w]*))?)/,
+  /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z\x80-\xff\u00a1-\uffff0-9.-]+|(?:www.|[-;:&=+$,\w]+@)[A-Za-z0-9\x80-\xff\u00a1-\uffff.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[\w]*))?)/,
 );
 
 const validateUrl = (url: string): boolean => {
