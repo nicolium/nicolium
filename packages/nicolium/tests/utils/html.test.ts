9a28f4f3d391a0a62c056c7ad690e8d4691d2727
nicole mikołajczyk <git@mkljczk.pl>
2026-08-25 23:52:17 +0000

    update test (assume this regression(?) is fine because safety)
    Signed-off-by: nicole mikołajczyk <git@mkljczk.pl>

--- packages/nicolium/tests/utils/html.test.ts
+++ packages/nicolium/tests/utils/html.test.ts
@@ -6,7 +6,7 @@ describe('html', () => {
   describe('unsecapeHTML', () => {
     it('returns unescaped HTML', () => {
       const output = html.unescapeHTML('<p>lorem</p><p>ipsum</p><br>&lt;br&gt;');
-      expect(output).toEqual('lorem\n\nipsum\n<br>');
+      expect(output).toEqual('lorem\n\nipsum\n\n\n<br>');
     });
   });
 });
